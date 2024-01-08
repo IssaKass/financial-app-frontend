d4bfb5b953720bbde446c212eba4fcb63eb2fd36
Abdallah-Issa Kass <issakass02@gmail.com>
2024-01-08 22:10:57 +0200

    added proxy

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>Choo Choo! This is an example of a Vite + React app running on Railway.</p>
-      </div>
-    </>
-  )
+	const [number, setNumber] = useState(0);
+
+	const handleClick = () => {
+		fetch("/api")
+			.then((res) => res.json())
+			.then((data) => setNumber(data.number))
+			.catch((error) => console.error(error));
+	};
+
+	return (
+		<>
+			<p>{import.meta.env.VITE_API_URL}</p>
+			<button onClick={handleClick}>Fetch number</button>
+			<p>{number}</p>
+		</>
+	);
 }
 
-export default App
+export default App;
